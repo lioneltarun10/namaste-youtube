864899af901725b15556fdacbbeeb60e22cd8ee0
lioneltarun10 <lioneltarun10@gmail.com>
2024-01-15 23:45:06 +0530

    Api call to youtube Api

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Button from './Button'
 
 const ButtonList = () => {
+  
+  const list = ["All","Gaming","Songs","Live","Cricket","Soccer","Valentines","Cooking","Data Structures"]
+  
   return (
     <div className='flex'>
-        <Button name="All"/>
-        <Button name="Gaming"/>
-        <Button name="Songs"/>
-        <Button name="Live"/>
-        <Button name="Cricket"/>
-        <Button name="Soccer"/>
-        <Button name="Cooking"/>
-        <Button name="Valentines"/>
+      {
+         list.map((btn) => <Button key={btn} name={btn}/>)
+      }
+      
+        
     </div>
   )
 }
